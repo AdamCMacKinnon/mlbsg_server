ab2a4e0d27f46fcd2f5760599eb491f2ac8c2c51
Adam C. MacKinnon <adamcmackdev@gmail.com>
2023-01-20 10:59:35 -0500

    small package update and created pick IDs logic

--- src/picks/picks.entity.ts
+++ src/picks/picks.entity.ts
@@ -1,30 +1,17 @@
 import { User } from '../auth/user.entity';
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Picks {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryColumn()
+  pickId: string;
 
   @Column()
   week: number;
 
   @Column()
   pick: string;
-
-  @CreateDateColumn({ type: 'timestamp', precision: 3 })
-  createdAt: Date;
-
-  @UpdateDateColumn({ type: 'timestamp', precision: 3 })
-  updatedAt: Date;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @ManyToOne((_type) => User, (user) => user.picks, { eager: false })
