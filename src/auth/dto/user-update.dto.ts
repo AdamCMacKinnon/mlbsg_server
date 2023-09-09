488c7b2bf69d10ba56ae7bb584c486e8a76d7728
Adam C. MacKinnon <adamcmackdev@gmail.com>
2023-09-08 20:08:43 -0400

    Randomized reset working.TODO: Email Logic

--- src/auth/dto/user-update.dto.ts
+++ src/auth/dto/user-update.dto.ts
@@ -1,21 +1,5 @@
-import {
-  IsString,
-  MinLength,
-  MaxLength,
-  Matches,
-  IsOptional,
-} from 'class-validator';
-
 export class UserUpdateDto {
   username?: string;
   email?: string;
-
-  @IsString()
-  @IsOptional()
-  @MinLength(8)
-  @MaxLength(32)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: `Password is too weak!  Must be at least 8 characters long and contain At least one Capital Letter, Number, and Special Character (such as #, ?, !, @, %, $, #)`,
-  })
-  password?: string;
+  password?: boolean;
 }
